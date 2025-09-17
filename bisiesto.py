996a27847d5ac95c06defa5a43327e256468dae1
EricPanas <eric.panas@iteso.mx>
2025-09-17 15:26:20 -0600

    primer intento

--- bisiesto.py
+++ bisiesto.py
@@ -1,15 +1,20 @@
 """
-Inserta el encabezado aquí y escribe tu código abajo
+Eric Fernando Panas López Dellamary
+
+17/09/2025
+
+Dado un año, regresa si es o no un año bisiesto.
 """
-
-# Declaraciones
-CONSTANTE = valor
-
 # Entradas
-entrada = input()
+anno = int(input("Introduzca un año: "))
 
 # Proceso
-
+if anno % 4 == 0 and anno % 100 != 0:
+    anno_bisiesto = "sí"
+elif anno % 400 == 0:
+    anno_bisiesto = "sí"
+else:
+    anno_bisiesto = "no"
 
 # Salidas
-print(salida)
+print(anno, anno_bisiesto, "es un año bisiesto")
